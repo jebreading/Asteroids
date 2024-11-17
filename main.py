14ef54f19c913000e1812544a0b6081292880c3b
jebreading <jebreading@gmail.com>
2024-11-17 11:30:32 +0000

    Player class and draw player in main

--- main.py
+++ main.py
@@ -2,25 +2,28 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from pygame.locals import *
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock = pygame.time.Clock()
     dt = 0
+    nplayer = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        pygame.Surface.fill(screen,(000,000,000))
+        screen.fill("black")
+        nplayer.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
         dt = Clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
