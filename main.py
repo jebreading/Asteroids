bcdce85cb5c6e0f8e7ffbe88786f91d6e4678ce6
jebreading <jebreading@gmail.com>
2024-11-17 10:41:26 +0000

    Added basic game loop and display

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            pygame.Surface.fill(screen,(000,000,000))
-            pygame.display.flip()
-
+            
+        pygame.Surface.fill(screen,(000,000,000))
+        pygame.display.flip()
+        
 if __name__ == "__main__":
     main()
